bcc12cf1afc5b199b227b61b9e9cfd25c5b91c64
suad <ss58452@ubt-uni.net>
2023-12-17 21:43:14 +0100

    DISCOUNT  CRUD

---
+++ EcommerceWeb/Data/Migrations/20231217160505_Suadi.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcommerceWeb.Data.Migrations
+{
+    public partial class Suadi : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ EcommerceWeb/Models/Disc.cs
@@ -0,0 +1,24 @@
+﻿// Models/Disc.cs
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceWeb.Models
+{
+    public class Discount
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        public decimal BeforeDiscountPrice { get; set; }
+
+        [Required]
+        public int DiscountPercentage { get; set; }
+
+        public decimal AfterDiscountPrice => BeforeDiscountPrice - (BeforeDiscountPrice * DiscountPercentage / 100);
+
+        public DateTime Date { get; set; } = DateTime.Now;
+
+    }
+}
+
